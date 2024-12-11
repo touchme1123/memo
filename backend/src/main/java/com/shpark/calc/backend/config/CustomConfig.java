0b81fc224427340936690797a145aa54ab6e3627
touchme1123 <touchme11309@gmail.com>
2024-12-11 14:52:24 +0900

    release test : CORS allowedOrigins

--- backend/src/main/java/com/shpark/calc/backend/config/CustomConfig.java
+++ backend/src/main/java/com/shpark/calc/backend/config/CustomConfig.java
@@ -10,7 +10,7 @@ public class CustomConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // 모든 경로에 대해 CORS 설정
-                .allowedOrigins("http://localhost:3000")  // 클라이언트에서 오는 요청을 허용
+                .allowedOrigins("https://simplez-board.netlify.app")  // 클라이언트에서 오는 요청을 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메소드 설정
                 .allowedHeaders("*");  // 모든 헤더 허용
     }
